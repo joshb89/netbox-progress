6e56404277d09707b0a92ccf47f141626f98d952
Josh Bonneville <jbonnevi@depaul.edu>
2023-12-13 08:05:58 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -243,7 +243,8 @@ if 'PAGINATE_COUNT' in environ:
     PAGINATE_COUNT = _environ_get_and_map('PAGINATE_COUNT', None, _AS_INT)
 
 # # Enable installed plugins. Add the name of each plugin to the list.
-# PLUGINS = []
+PLUGINS = [ 'phonebox_plugin', 
+]
 
 # # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
