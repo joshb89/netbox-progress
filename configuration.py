0fdaaea3e40a4ee0ddac7289bcfde045684d2aa0
Josh Bonneville <jbonnevi@depaul.edu>
2023-12-14 09:35:15 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -243,13 +243,19 @@ if 'PAGINATE_COUNT' in environ:
     PAGINATE_COUNT = _environ_get_and_map('PAGINATE_COUNT', None, _AS_INT)
 
 # # Enable installed plugins. Add the name of each plugin to the list.
-PLUGINS = [ 'phonebox_plugin', 
+PLUGINS = [ 'phonebox_plugin', 'netbox_acls',
 ]
 
 # # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
-# PLUGINS_CONFIG = {
-# }
+ PLUGINS_CONFIG = {
+   "netbox_acls": {
+        "top_level_menu": True # If set to True the plugin will add a top level menu item for the plugin. If set to False the plugin will add a menu item under the Plugins menu item.  Default is set to True.
+    },
+}
+
+
+ }
 
 # When determining the primary IP address for a device, IPv6 is preferred over IPv4 by default. Set this to True to
 # prefer IPv4 instead.
